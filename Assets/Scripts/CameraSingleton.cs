c07bb9638ee64354fce22431bbdb0c402a610b8f
ekaterina.kuznetsova <ekaterina.kuznetsova@jetbrains.com>
2023-01-19 11:36:07 +0200

    Add SerializeField suggestion check

--- Assets/Scripts/CameraSingleton.cs
+++ Assets/Scripts/CameraSingleton.cs
@@ -1,7 +1,13 @@
 // There are many ways of getting access to the main camera, but the approach using
 // a singleton (as we use here) works for any kind of MonoBehaviour.
+
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
 class CameraSingleton : UnityEngine.MonoBehaviour
 {
+    [SerializeField]
     public static UnityEngine.Camera Instance;
 
     void Awake()
